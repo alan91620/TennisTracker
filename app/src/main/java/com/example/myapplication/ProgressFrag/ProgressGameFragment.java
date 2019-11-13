9c2c9252063ba689804f32024d5c98e55dcf9203
Milkad0 <44178372+Milkad0@users.noreply.github.com>
2019-11-13 03:52:26 +0100

    Not working location

--- app/src/main/java/com/example/myapplication/ProgressFrag/ProgressGameFragment.java
+++ app/src/main/java/com/example/myapplication/ProgressFrag/ProgressGameFragment.java
@@ -1,5 +1,8 @@
 package com.example.myapplication.ProgressFrag;
 
+import android.content.Context;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +12,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+import static androidx.core.content.ContextCompat.getSystemService;
 
 
 public class ProgressGameFragment extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+
         return inflater.inflate(R.layout.fragment_progress,null);
     }
+
+
 }
+
