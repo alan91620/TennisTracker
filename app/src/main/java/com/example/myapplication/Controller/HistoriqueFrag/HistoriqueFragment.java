e5112aa54a1b1becf820f284c11dc5d05ae85d45
Milkad0 <44178372+Milkad0@users.noreply.github.com>
2019-11-26 23:33:53 +0100

    Update Histo

--- app/src/main/java/com/example/myapplication/Controller/HistoriqueFrag/HistoriqueFragment.java
+++ app/src/main/java/com/example/myapplication/Controller/HistoriqueFrag/HistoriqueFragment.java
@@ -2,10 +2,12 @@ package com.example.myapplication.Controller.HistoriqueFrag;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,19 +27,20 @@ public class HistoriqueFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         recupMatch();
 
-        
+        //Name = getView().findViewById(R.id.textViewJ1Name1);
+        recupMatch();
         return inflater.inflate(R.layout.fragment_historique,null);
     }
 
 
 
-
+//Récupère ce qu'il y a sur la bddLite
     private void recupMatch(){
 
         accesLocal = new AccesLocal(this.getContext());
         historyMatch =  accesLocal.recupDernier();
-
-
+        Log.d("HISTOTRUC",historyMatch.getJoueur1LastName());
+        //Name.setText("test");
     }
 
 
--- app/src/main/java/com/example/myapplication/Model/HistoryMatch.java
+++ app/src/main/java/com/example/myapplication/Model/HistoryMatch.java
@@ -21,15 +21,32 @@ public class HistoryMatch {
         this.joueur2LastName = joueur2LastName;
         this.adresseMatch = streetName;
         GameHist = new ArrayList<>();
-        GameHist.set(0,Score.get(0));
-        GameHist.set(1,Score.get(1));
-        GameHist.set(2,Score.get(2));
-        GameHist.set(3,Score.get(3));
-        GameHist.set(4,Score.get(4));
-        GameHist.set(5,Score.get(5));
-        GameHist.set(6,Score.get(6));
-        GameHist.set(7,Score.get(7));
-        GameHist.set(8,Score.get(8));
-        GameHist.set(9,Score.get(9));
+
+    }
+
+    public String getJoueur1FirstName() {
+        return joueur1FirstName;
+    }
+
+    public String getJoueur2FirstName() {
+        return joueur2FirstName;
+    }
+
+    public String getJoueur1LastName() {
+        return joueur1LastName;
+    }
+
+    public String getJoueur2LastName() {
+        return joueur2LastName;
+    }
+
+    public String getAdresseMatch() {
+        return adresseMatch;
+    }
+
+    public ArrayList<String> getGameHist() {
+        return GameHist;
     }
 }
+
+
